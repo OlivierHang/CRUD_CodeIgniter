986ad6f2dc0606c67cfb8e67f23b3b965abebd6e
Olivier Hang <hang.sengolivier@gmail.com>
2020-12-17 12:17:33 +0100

    Fonction des Sessions tester avec succes dans le controller 'Home.php' avec les 3 methodes 'setSession', 'getSession' et 'destroySession'

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,10 +1,42 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Home extends BaseController
 {
 	public function index()
 	{
 		return view('welcome_message');
+	}
+
+	public function setSession($nom, $id)
+	{
+		$maSession = session();
+		$monArray = [
+			'nom' => $nom,
+			'id' => $id,
+		];
+		$maSession->set($monArray);
+		return redirect()->to(base_url());
+	}
+
+	public function getSession()
+	{
+		$maSession = session();
+		if ($maSession->has('nom') && $maSession->has('id')) {
+			$nom = $maSession->get('nom');
+			$id = $maSession->get('id');
+			echo $nom . ' ' . $id;
+		} else {
+			echo 'Pas de nom dans la session';
+		}
+	}
+
+	public function destroySession()
+	{
+		$maSession = session();
+		$maSession->destroy();
+		return redirect()->to(base_url());
 	}
 
 	//--------------------------------------------------------------------
